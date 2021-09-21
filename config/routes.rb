b94f16039fa5b4f4a10792f46a876e96ccc330c5
elmo0509 <elmo.boof@gmail.com>
2021-09-21 14:50:07 +0000

    [Fix]ユーザログイン前

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#show'
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
 end
