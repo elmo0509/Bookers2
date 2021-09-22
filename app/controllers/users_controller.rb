227147179801f61e5fcac794b85d52891a907397
elmo0509 <elmo.boof@gmail.com>
2021-09-22 18:12:41 +0000

    [Add]ログイン後のHome遷移画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+    @newbook = Book.new
+    @books = Book.all
   end
 
   def edit
