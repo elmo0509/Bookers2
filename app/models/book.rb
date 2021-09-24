529f32c7cac199d1c7bac27784e2bc07b9866c98
elmo0509 <elmo.boof@gmail.com>
2021-09-24 07:50:55 +0000

    [Add]bookモデルにバリデーション設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
 
   belongs_to :user
+  
+  # バリデーション設定
+  validates :title, presence: true
+  
+  validates :body,
+    presence: true,
+    length: {maximum: 20}
 
 end
